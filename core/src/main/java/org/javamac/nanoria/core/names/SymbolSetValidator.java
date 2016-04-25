ad2da945be5aef6973bc00e6e241fc124f04ac07
FDG <fdg@javamac.com>
2016-04-25 14:28:28 +0200

    Validator correction to match second test case

--- core/src/main/java/org/javamac/nanoria/core/names/SymbolSetValidator.java
+++ core/src/main/java/org/javamac/nanoria/core/names/SymbolSetValidator.java
@@ -1,39 +1,49 @@
 package org.javamac.nanoria.core.names;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 public class SymbolSetValidator {
+    private final FollowingRolesMap followingRolesMap = new FollowingRolesMap();
+    private final SymbolSet symbols;
     private final boolean valid;
 
     public SymbolSetValidator(SymbolSet symbols) {
-        valid = validate(symbols);
+        this.symbols = symbols;
+        this.valid = validate();
     }
 
     public boolean isValid() {
         return valid;
     }
 
-    private boolean validate(SymbolSet symbols) {
+    private boolean validate() {
         List<Symbol> allSymbols = symbols.getAllSymbols();
         for (Symbol symbol : allSymbols) {
-            if (!validate(symbol, symbols)) {
+            if (!validate(symbol)) {
                 return false;
             }
         }
         return allSymbols.size() > 0;
     }
 
-    private boolean validate(Symbol symbol, SymbolSet symbols) {
+    private boolean validate(Symbol symbol) {
         Iterator<Role[]> i = new CompatibleRolesIterator(symbol.getRoles());
         while (i.hasNext()) {
-            Role[] compatibleRoles = i.next();
-            List<Symbol> matchingSymbols = symbols.selectMatchingSymbols(symbol, compatibleRoles);
-            if (matchingSymbols.size() == 0) {
-                return false;
-            }
+            if (validateForRoles(symbol, i.next())) return false;
         }
         return true;
+    }
+
+    private boolean validateForRoles(Symbol symbol, Role[] compatibleRoles) {
+        for(Role[] followingRoles: followingRolesMap.getFollowingRoles(compatibleRoles)) {
+            List<Symbol> matchingSymbols = symbols.selectMatchingSymbols(symbol, followingRoles);
+            if (matchingSymbols.size() == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
